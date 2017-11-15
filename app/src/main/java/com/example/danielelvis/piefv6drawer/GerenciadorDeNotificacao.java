b303824b230f3100f845aafeec32575a6df9880b
dequevedo <danielelvisquevedo@gmail.com>
2017-11-15 20:41:21 -0200

    notificacao

--- app/src/main/java/com/example/danielelvis/piefv6drawer/GerenciadorDeNotificacao.java
+++ app/src/main/java/com/example/danielelvis/piefv6drawer/GerenciadorDeNotificacao.java
@@ -1,16 +1,22 @@
 package com.example.danielelvis.piefv6drawer;
 
-public class GerenciadorDeNotificacao {
-    private static final GerenciadorDeNotificacao ourInstance = new GerenciadorDeNotificacao();
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
 
+public class GerenciadorDeNotificacao extends AppCompatActivity {
+
+    private static final GerenciadorDeNotificacao ourInstance = new GerenciadorDeNotificacao();
     public static GerenciadorDeNotificacao getInstance() {
         return ourInstance;
     }
+    private GerenciadorDeNotificacao() {}
 
-    private GerenciadorDeNotificacao() {
-    }
-
-    public void enviarNotificacao(){
-        //Enviar Notificação utilizando o Firebase da google
-    }
 }
